61ddf025a54526937a5e35b1425a82eaf2ac41d0
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2020-02-14 22:32:31 +0900

    Added Grid layout
    Used Material UI Grid to position components correctly

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -3,28 +3,48 @@ import { makeStyles } from "@material-ui/core/styles";
 import SearchBar from "material-ui-search-bar";
 import ProjectList from "../components/ProjectList";
 import SideBar from "../components/SideBar";
-const useStyles = makeStyles({
-  wrapper: {
+
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
     position: "relative",
     top: 0,
     height: "100vh"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
   }
-});
+}));
 
 const Dashboard = props => {
   const classes = useStyles();
   return (
-    <div className={classes.wrapper}>
-      <SideBar />
-      <SearchBar
-        onChange={() => console.log("onChange")}
-        onRequestSearch={() => console.log("onRequestSearch")}
-        style={{
-          margin: "0 auto",
-          maxWidth: 800
-        }}
-      />
-      <ProjectList />
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Paper>
+            <SideBar />
+          </Paper>
+        </Grid>
+        <Grid item xs={9}>
+          <Paper>
+            <SearchBar
+              onChange={() => console.log("onChange")}
+              onRequestSearch={() => console.log("onRequestSearch")}
+              style={{
+                margin: "0 auto",
+                maxWidth: 800
+              }}
+            />
+            <ProjectList />
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
