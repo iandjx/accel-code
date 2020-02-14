df867041593f0676f8b87c761ea103de117f21aa
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2020-02-14 10:43:49 +0900

    Imported Project Card to ProjecList

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Button from "@material-ui/core/Button";
 import Search from "./components/SearchBar";
 import SearchBar from "material-ui-search-bar";
 import ProjectCard from "./components/ProjectCard";
+import ProjectList from "./components/ProjectList";
 const App = () => (
   <div>
     <SearchBar />
-    <ProjectCard />
+    <ProjectList />
   </div>
 );
 export default App;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,7 +1,19 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
+const techStack = ["MQL5", "Python", "ReactJS"];
 
 const ProjectList = () => {
-  return null;
+  return (
+    <ProjectCard
+      title="MQL5"
+      description="This forex trading robot can take trades based on several indicators
+  such as bollinger, moving averages, RSI. Grid and Martingale trading
+  can be added but not a priority. Lot trading should be managed as
+  well."
+      contributers="5"
+      techStack={techStack}
+    />
+  );
 };
 
 export default ProjectList;
